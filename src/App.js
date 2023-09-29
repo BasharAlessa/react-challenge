5e3fd346239d280e5f9592cb07c370146b5ddae8
ESSA <eng.bashar.alessa@gmail.com>
2023-09-29 23:23:53 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import GitAcount from "./components/GitAcount"
+import GitRepo from "./components/GitRepo";
+import "./App.css"
 function App() {
+  const [username, setUsername] = useState("");
+  const [clientId, setClientId] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleClientIdChange = (e) => {
+    setClientId(e.target.value);
+  };
+
+  const handleClientSecretChange = (e) => {
+    setClientSecret(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Github User</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter GitHub Username:
+          <input
+            type="text"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </label>
+        <label>
+          Enter Your Client ID:
+          <input
+            type="text"
+            value={clientId}
+            onChange={handleClientIdChange}
+          />
+        </label>
+        <label>
+          Enter Your Client Secret:
+          <input
+            type="text"
+            value={clientSecret}
+            onChange={handleClientSecretChange}
+          />
+        </label>
+        <button type="submit">submit</button>
+      </form>
+      <GitAcount username={username} clientId={clientId} clientSecret={clientSecret} />
+      {username && <GitRepo username={username} clientId={clientId} clientSecret={clientSecret} />}
     </div>
   );
 }
